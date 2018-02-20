4ddc82e76094f0cd084447d275798da0febd6085
Kosara Golemshinska <gusgolko@student.gu.se>
2018-02-20 15:21:00 +0100

    added multicast test code

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,14 @@ int main(int argc, char **argv) {
 	std::cout << "Let's test this" << std::endl;
 
     
-    cluon::UDPSender sender{"127.0.0.1", 1234};
+    cluon::UDPSender sender{"225.0.0.111", 1236};
     sender.send("Sending!");
+    
+    
+    using namespace std::literals::chrono_literals;
+    std::this_thread::sleep_for(5s);
 
-    cluon::UDPReceiver receiver("0.0.0.0", 1235,
+    cluon::UDPReceiver receiver("225.0.0.111", 1236,
         [](std::string &&data, std::string &&/*from*/,
            std::chrono::system_clock::time_point &&/*timepoint*/) noexcept {
         std::cout << "Received " << data.size() << " bytes." << std::endl;
