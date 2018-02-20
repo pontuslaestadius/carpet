c7925b25200f7231d3356d398fbe9f41fb935fc2
Kosara Golemshinska <gusgolko@student.gu.se>
2018-02-20 14:39:39 +0100

    test changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
-#include<iostream> 
+#include <chrono>
+#include <iostream> 
+
+#include "cluon/UDPSender.hpp"
+#include "cluon/UDPReceiver.hpp"
 int main(int argc, char **argv) {
-	std::cout << "Hello World!" << std::endl;
+	std::cout << "Let's test this" << std::endl;
+
+    
+    cluon::UDPSender sender{"127.0.0.1", 1234};
+    sender.send("Sending!");
+
+    cluon::UDPReceiver receiver("0.0.0.0", 1235,
+        [](std::string &&data, std::string &&/*from*/,
+           std::chrono::system_clock::time_point &&/*timepoint*/) noexcept {
+        std::cout << "Received " << data.size() << " bytes." << std::endl;
+    });
+
+    using namespace std::literals::chrono_literals;
+    while (receiver.isRunning()) {
+        std::this_thread::sleep_for(1s);
+    }    
+    
   	return 0;
 }
 
