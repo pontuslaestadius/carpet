938ed4b8c05e4a7b5184c73fda571dd3d8ece35f
PontusLaestadius <pontus.laestadius@gmail.com>
2018-03-06 10:56:48 +0100

    uses chars

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,27 @@
 #include <iostream>
 #include <sstream>
 #include <thread>
+#include <unistd.h>
 
 #include "cluon/OD4Session.hpp"
 #include "cluon/Envelope.hpp"
 
 #include "messages.hpp"
 
-int main(int argc, char** argv) {
+
+	opendlv::proxy::GroundSteeringReading msgSteering;
+	opendlv::proxy::PedalPositionReading msgPedal;
+
+	double angle = 0.0;
+	double speed = 0.0;
+
+void action(char &p);
+
+void safety_check();
+void print_debug();
+void set_msg();
+
+int main(int /*argc*/, char** /*argv*/) {
 
     cluon::OD4Session od4(111,[](cluon::data::Envelope &&envelope) noexcept {
         if (envelope.dataType() == opendlv::proxy::GroundSteeringReading::ID()) {
@@ -27,49 +41,59 @@ int main(int argc, char** argv) {
         std::cout << "ERROR: No od4 running!!!" << std::endl;
         return -1;
     }
+	
+	char input;
 
-	opendlv::proxy::GroundSteeringReading msgSteering;
-	opendlv::proxy::PedalPositionReading msgPedal;
-
-	double angle = 0.0;
-	double speed = 0.0;
-
-	for (char **a = argv ; a != argv+argc ; a++) {
-    	for(char *p = *a ; *p != '\0' ; p++) {
-
-		    if (speed >= 0.0)
-				speed -= 0.1;
-
-		    switch (*p) {
-		        case 'w':
-		            speed += 0.1;
-					continue;
-		            break;
-		        case 't':
-					angle -= 10.0;
-		            break;
-		        case 'y':
-		            angle += 10.0;
-			    break;
-		        default:
-		        std::cout << "Invalid: " << *argv << std::endl;
-		            return 0;
-		    }
-		    for (int i = 0; i < INT32_MAX; i++);
-
-			if (angle > 30) angle = 30;
-			if (angle < -30) angle = -30;
-			if (speed > 0.25) speed = 0.25;
-
-			std::cout << "(speed, angle) -> (" << speed << "," << angle << ")" << std::endl;
-
-		    msgPedal.percent(speed);
-		    msgSteering.steeringAngle(angle);
-		    od4.send(msgPedal);
-		    od4.send(msgSteering);
-    	}
+	while ((input = getchar()) != 'q') {
+		action(input);
+		od4.send(msgPedal);
+		od4.send(msgSteering);
+		usleep(1000000000);
 	}
 
+    msgPedal.percent(0.0);
+    msgSteering.steeringAngle(0.0);
+    od4.send(msgPedal);
+    od4.send(msgSteering);
+
     return 0;
+}
+
+void action(char &p) {
+	if (speed >= 0.0)
+		speed -= 0.1;
+
+	switch (p) {
+	case 'w':
+	    speed += 0.1;
+	    break;
+	case 't':
+	    angle -= 10.0;
+	    break;
+	case 'y':
+	    angle += 10.0;
+	    break;
+	default:
+		std::cout << "Invalid: " << p << std::endl;
+	}
+
+	safety_check();
+	print_debug();
+	set_msg();
+}
+
+void print_debug() {
+	std::cout << "(speed, angle) -> (" << speed << "," << angle << ")" << std::endl;
+}
+
+void safety_check() {
+	if (angle > 30) angle = 30;
+	if (angle < -30) angle = -30;
+	if (speed > 0.25) speed = 0.25;
+}
+
+void set_msg() {
+    msgPedal.percent(speed);
+    msgSteering.steeringAngle(angle);
 }
 
