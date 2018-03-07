51eeb7312020426338384db05c5aa91923c87eff
PontusLaestadius <pontus.laestadius@gmail.com>
2018-03-07 09:05:44 +0100

    updates chain with: transfer, ba, help, refactors, removes default options.

--- chain.sh
+++ chain.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash""g
 
 #################################
 ### Author: Pontus Laestadius ###
@@ -15,7 +15,7 @@
 # Default to enable building.
 HELP=".CHAIN_HELP"
 FORCE="no"
-DOCKER_NAME="carpet_toolchain"
+DOCKER_NAME="carpet_compile"
 DOCKER_IMAGE="pontusla/carpet_compile"
 
 # Command-line argument
@@ -23,108 +23,117 @@ for i in "$@"
 do
 case $i in
 
-	--pull)
-	docker pull $DOCKER_IMAGE
-	exit
-	shift # past argument=value
-    	;;
- 
-	--push)
-	DOCKER_ID_USER=$2
-
-	# Bind the local with the remote.
-	docker tag carpet_compile $DOCKER_ID_USER/carpet_compile
-
-	# Push them to the hub.
-	docker push $DOCKER_ID_USER/carpet_compile
-    	exit
-	shift
-	;;
-
-	-b|--build)
-    docker build -t carpet_compile .
-    exit
-
-   shift # past argument=value
+	-t|--transfer)
+	
+			docker save carpet_compile.tar carpet_compile:armhf
+			scp carpet_compile.tar debian@192.168.8.1:~/
+			rm carpet_compile.tar
+			ssh debian@192.168.8.1 -t 'sudo sh run.sh'
+	
     ;;
+	--run)
+
+			# If a container already exists and is running.
+			if [ "$(docker ps -aq -f status=running -f name=$DOCKER_NAME)" ]; then
+
+				# Without force, prompt the user with a choice.
+				if [ "${FORCE}" = "no" ];
+					then
+						# clean up with prompt, only works with bash and often not user shell.
+						echo "The container is already running. Do you want to continue? [Y/N]:"
+						read -n 1 REPLY
+						echo    # (optional) move to a new line
+
+					else # In force, the user has no option.
+						REPLY="Y" 
+					fi 
+
+
+				if [[ $REPLY =~ ^[Yy]$ ]];	# Use confirmation. Needs BASH not user shell.
+				then
+					docker stop $DOCKER_NAME
+				else
+					exit
+				fi
+
+			fi
+
+			# If a container already exists.
+			if [ "$(docker ps -aq -f status=exited -f name=$DOCKER_NAME)" ]; then
+				# cleanup
+				docker rm $DOCKER_NAME
+			fi
+
+			docker run -d --net=host --name $DOCKER_NAME -v $PWD:/opt/sources $DOCKER_IMAGE /bin/sh
+
+    ;;
+	--pull)
+	
+			docker pull $DOCKER_IMAGE
+
+    ;;
+	--push)
+	
+			DOCKER_ID_USER=$2
+			# Bind the local with the remote.
+			docker tag $DOCKER_NAME $DOCKER_ID_USER/$DOCKER_NAME
+			# Push them to the hub.
+			docker push $DOCKER_ID_USER/$DOCKER_NAME
+
+	;;
+	-b|--build)
+    	
+    			docker build -t carpet_compile .
+
+	;;
+	-ba|--build-arm)
+    	
+    			docker build -t carpet_compile:armhf -f Dockerfile.armhf .
+
+        ;;
 	--debug)
 
-	valgrind --tool=memcheck --log-file=".valgrind_output" --leak-check=yes ./build/carpet
-	exit
-	shift
+			valgrind --tool=memcheck --log-file=".valgrind_output" --leak-check=yes ./build/carpet
+
 	;;
     -l|--local)
-	if [ ! -d "build" ]; then
-  	# Control will enter here if $DIRECTORY doesn't exist.
-		mkdir build
-	fi
+    
+			DIR="build"
+			if [ ! -d "$DIR" ]; then
+				mkdir $DIR
+			else
+				rm $DIR -rf
+			fi
+			cd $DIR
+			cmake -D CMAKE_BUILD_TYPE=Release .. && \
+			ctest -T memcheck && \
+			make
 
-	cd build # non-failable action due to previous check.
-	cmake -D CMAKE_BUILD_TYPE=Release .. && \
-	ctest -T memcheck && \
-	make
-	exit
-
-    shift # past argument=value
     ;;
     -f|--force)
-    FORCE="yes"
-    shift # past argument=value
+    
+    			FORCE="yes"
+    
     ;;
     -d|--delete)
     
-    docker rm carpet_toolchain
-    exit
+    			docker rm $DOCKER_NAME
     
-    shift # past argument=value
     ;;
     -h|--help)
-	cat $HELP
-    exit
-    shift # past argument=value
+    
+			cat $HELP
+			
     ;;
     *)
-    echo "Unknown argument: '$i'"
-          # unknown option
+    #	Invalid argument
+    
+			echo "Unknown argument: '$i'"
+			cat $HELP
+			
+	exit 1
     ;;
 esac
 done
 
-# Runs a docker container.
-# If a container already exists and is running.
-if [ "$(docker ps -aq -f status=running -f name=$DOCKER_NAME)" ]; then
 
-    # Without force, prompt the user with a choice.
-    if [ "${FORCE}" = "no" ];
-        then
-            # clean up with prompt
-            echo "The container is already running. Do you want to continue? [Y/N]:"
-            read -n 1 REPLY
-            echo    # (optional) move to a new line
-
-        else # In force, the user has no option.
-            REPLY="Y" 
-        fi 
-
-	# Use confirmation. Needs BASH not user shell.
-    if [[ $REPLY =~ ^[Yy]$ ]];
-    then
-        docker stop $DOCKER_NAME
-    else
-        exit
-    fi
-
-fi
-
-# If a container already exists.
-if [ "$(docker ps -aq -f status=exited -f name=$DOCKER_NAME)" ]; then
-    # cleanup
-    docker rm $DOCKER_NAME
-fi
-
-# run your container:
-# deletes container on exit.
-# deletes container on host boot.
-# set name to $DOCKER_NAME.
-# mount the volume in current directory.
-docker run -d --net=host --name $DOCKER_NAME -v $PWD:/opt/sources $DOCKER_IMAGE /bin/sh
