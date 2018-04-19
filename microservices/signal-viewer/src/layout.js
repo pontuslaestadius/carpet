e91da4e7f4df6cdfe4e295db8d534d0d6e5a09b8
PontusLaestadius <pontus.laestadius@gmail.com>
2018-04-19 12:01:03 +0200

    fixed typo

--- microservices/signal-viewer/src/layout.js
+++ microservices/signal-viewer/src/layout.js
@@ -1,7 +1,7 @@
  // KeyCode or 'c'haracter, field, messageid, [down value, up value], label.
 registerkey(); // This is not displayed
 //registerkey(38, "forward", 1041, [0.25, 0.00]);
-registerkey(38, "forward", 1041, ["range", 0.00]);
+registerkey(38, "percent", 1041, ["range", 0.00]);
 registerkey(); // This is not displayed
 registerkey(37, "steeringAngle", 1045, [-15, 0], "left");
 registerkey(40, "reverse", 1041, [-0.25, 0.00]);
